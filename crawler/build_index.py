3d15cb993db4fed21883be9d65d9ed0605c3d451
vul358 <b00303062@ntu.edu.tw>
2023-09-24 19:48:29 +0800

    add daily crawler

--- crawler/build_index.py
+++ crawler/build_index.py
@@ -1,11 +1,23 @@
 from elasticsearch import Elasticsearch
 import json
-from django.conf import settings
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+es_host = os.getenv('ES_host')
+es_usr = os.getenv('ES_usr')
+es_psw = os.getenv('ES_psw')
+es_ssl = os.getenv('ES_ssl')
+es_cert = os.getenv('ES_cert')
+
 
 es = Elasticsearch(
-    f"https://{settings.es_host}",
-    ssl_assert_fingerprint= settings.es_ssl,
-    basic_auth=(settings.es_usr, settings.es_psw)
+    f"https://{es_usr}:{es_psw}@{es_host}",
+    # ssl_assert_fingerprint= es_ssl,
+    # basic_auth=(es_usr, es_psw),
+    ca_certs = es_cert,
+    verify_certs = True
     )
 
 
@@ -27,7 +39,7 @@ def get_setting():
     return settings
 
 
-def mappings():
+def get_mappings():
     mappings = {
         "properties": {
             "title": {
