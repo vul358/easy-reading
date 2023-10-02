685eda8d79675b6a201f5bc0b6c8311c5d02f4c3
vul358 <b00303062@ntu.edu.tw>
2023-10-02 15:33:02 +0800

    try to add allowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','35.74.46.248']
 
 
 # Application definition
