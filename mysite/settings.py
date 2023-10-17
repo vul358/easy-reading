4ac5b524c2ae3494ff90d0345b2e1617a5659522
vul358 <b00303062@ntu.edu.tw>
2023-10-17 15:47:02 +0800

    test health_check

--- mysite/settings.py
+++ mysite/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'novels.middleware.HealthCheckMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
